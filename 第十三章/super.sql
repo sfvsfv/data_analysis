2960aa6faf0503efd51b92b979a84743c9528cc6
菲菲 <62045791+sfvsfv@users.noreply.github.com>
2024-01-01 05:15:53 +0800

    Update super.sql

--- 第十三章/super.sql
+++ 第十三章/super.sql
@@ -1,19 +1,27 @@
-INSERT INTO `super` VALUES ('泡面', 5.00, 2);
-INSERT INTO `super` VALUES ('牛奶', 3.00, 3);
-INSERT INTO `super` VALUES ('辣条', 3.00, 4);
-INSERT INTO `super` VALUES ('矿泉水', 2.00, 5);
-INSERT INTO `super` VALUES ('方便面', 2.50, 6);
-INSERT INTO `super` VALUES ('脉动', 5.00, 7);
-INSERT INTO `super` VALUES ('面包', 4.00, 8);
-INSERT INTO `super` VALUES ('早餐奶', 5.50, 9);
-INSERT INTO `super` VALUES ('洗面奶', 30.00, 10);
-INSERT INTO `super` VALUES ('洗发水', 35.00, 11);
-INSERT INTO `super` VALUES ('沐浴露', 42.00, 12);
-INSERT INTO `super` VALUES ('公牛插座', 80.00, 13);
-INSERT INTO `super` VALUES ('梳子', 12.00, 14);
-INSERT INTO `super` VALUES ('蓝牙', 80.00, 15);
-INSERT INTO `super` VALUES ('蛋糕', 12.00, 17);
-INSERT INTO `super` VALUES ('鸡腿', 6.50, 8);
-INSERT INTO `super` VALUES ('计算机网络', 35.00, 19);
-INSERT INTO `super` VALUES ('数据库原理', 37.50, 20);
-INSERT INTO `super` VALUES ('大数据', 37.00, 21);
+CREATE TABLE test.super (
+  `name` VARCHAR(255) NOT NULL,
+  `price` DECIMAL(10, 2) NOT NULL,
+  `ID` INT NOT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+
+INSERT INTO test.super VALUES ('泡面', 5.00, 2);
+INSERT INTO test.super VALUES ('牛奶', 3.00, 3);
+INSERT INTO test.super VALUES ('辣条', 3.00, 4);
+INSERT INTO test.super VALUES ('矿泉水', 2.00, 5);
+INSERT INTO test.super VALUES ('方便面', 2.50, 6);
+INSERT INTO test.super VALUES ('脉动', 5.00, 7);
+INSERT INTO test.super VALUES ('面包', 4.00, 8);
+INSERT INTO test.super VALUES ('早餐奶', 5.50, 9);
+INSERT INTO test.super VALUES ('洗面奶', 30.00, 10);
+INSERT INTO test.super VALUES ('洗发水', 35.00, 11);
+INSERT INTO test.super VALUES ('沐浴露', 42.00, 12);
+INSERT INTO test.super VALUES ('公牛插座', 80.00, 13);
+INSERT INTO test.super VALUES ('梳子', 12.00, 14);
+INSERT INTO test.super VALUES ('蓝牙', 80.00, 15);
+INSERT INTO test.super VALUES ('蛋糕', 12.00, 17);
+INSERT INTO test.super VALUES ('鸡腿', 6.50, 18);
+INSERT INTO test.super VALUES ('计算机网络', 35.00, 19);
+INSERT INTO test.super VALUES ('数据库原理', 37.50, 20);
+INSERT INTO test.super VALUES ('大数据', 37.00, 21);
